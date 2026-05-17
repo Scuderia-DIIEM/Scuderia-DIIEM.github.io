c9323f8efc7380f6708accc672cb7e94711bbf82
Jacopo <jacopo1.evangelisti@gmail.com>
2026-05-17 13:43:46 +0200

    Initial deploy

---
+++ _next/static/MG9V_TxmWsUbE4y5qXwYc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/935-bd222b5085c6ff19.js","static/chunks/pages/index-22ad1a377d0eb067.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
